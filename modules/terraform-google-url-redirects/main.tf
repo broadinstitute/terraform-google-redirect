3b56e0909a405b3fb649f3e932b370ee21bc1349
Andrew Teixeira <teixeira@broadinstitute.org>
2025-07-25 12:54:04 -0400

    Initial commit

---
+++ modules/terraform-google-url-redirects/main.tf
@@ -0,0 +1,98 @@
+locals {
+  safe_name = var.name != null ? var.name : replace(var.hostnames[0], ".", "-")
+}
+
+resource "google_compute_url_map" "https_url_map" {
+  name = "${local.safe_name}-https-url-map"
+
+  default_url_redirect {
+    host_redirect          = var.default_destination_host
+    path_redirect          = var.default_destination_path
+    redirect_response_code = "TEMPORARY_REDIRECT"
+    strip_query            = false
+  }
+
+  host_rule {
+    hosts        = var.hostnames
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+
+    default_url_redirect {
+      host_redirect          = var.default_destination_host
+      path_redirect          = var.default_destination_path
+      redirect_response_code = "TEMPORARY_REDIRECT"
+      strip_query            = false
+    }
+
+    dynamic "path_rule" {
+      for_each = var.redirects
+      iterator = rule
+
+      content {
+        paths = rule.value.source_paths
+
+        url_redirect {
+          https_redirect         = true
+          host_redirect          = rule.value.destination_host
+          path_redirect          = rule.value.destination_path
+          redirect_response_code = rule.value.redirect_response_code
+          strip_query            = false
+        }
+      }
+    }
+  }
+}
+
+resource "google_compute_managed_ssl_certificate" "certificate" {
+  name     = "${local.safe_name}-managed-certificate"
+  provider = google
+
+  managed {
+    domains = var.hostnames
+  }
+}
+
+resource "google_compute_target_https_proxy" "https_proxy" {
+  name             = "${local.safe_name}-https-proxy"
+  ssl_certificates = [google_compute_managed_ssl_certificate.certificate.self_link]
+  ssl_policy       = var.ssl_policy
+  url_map          = google_compute_url_map.https_url_map.self_link
+}
+
+resource "google_compute_url_map" "http_to_https_redirect" {
+  name = "${local.safe_name}-to-http-url-map"
+
+  default_url_redirect {
+    https_redirect         = true
+    redirect_response_code = "MOVED_PERMANENTLY_DEFAULT"
+    strip_query            = false
+  }
+}
+
+resource "google_compute_target_http_proxy" "http_proxy" {
+  name    = "${local.safe_name}-http-proxy"
+  url_map = google_compute_url_map.http_to_https_redirect.self_link
+}
+
+resource "google_compute_global_address" "public_address" {
+  address_type = "EXTERNAL"
+  ip_version   = "IPV4"
+  name         = "${local.safe_name}-public-address"
+}
+
+resource "google_compute_global_forwarding_rule" "global_forwarding_https_rule" {
+  ip_address = google_compute_global_address.public_address.address
+  name       = "${local.safe_name}-global-forwarding-https-rule"
+  port_range = "443"
+  target     = google_compute_target_https_proxy.https_proxy.self_link
+}
+
+resource "google_compute_global_forwarding_rule" "global_forwarding_http_rule" {
+  ip_address = google_compute_global_address.public_address.address
+  name       = "${local.safe_name}-global-forwarding-http-rule"
+  port_range = "80"
+  target     = google_compute_target_http_proxy.http_proxy.self_link
+}
---
+++ modules/terraform-google-url-redirects/variables.tf
@@ -0,0 +1,41 @@
+variable "hostnames" {
+  description = "The list of hostname(s) the redirects will apply to."
+  type        = list(string)
+}
+
+variable "default_destination_host" {
+  description = "The default host to redirect to if no paths are matched."
+  type        = string
+}
+
+variable "default_destination_path" {
+  description = "The default path to redirect to if no paths are matched."
+  type        = string
+}
+
+variable "name" {
+  default     = null
+  description = "The name to use for all resources created."
+  nullable    = true
+  type        = string
+  validation {
+    condition     = can(regex("^[a-z][a-z0-9-]*$", var.name)) || var.name == null
+    error_message = "The name value must be a valid Google resource name, alphanumeric and dashes."
+  }
+}
+
+variable "redirects" {
+  description = "The map of redirects to apply to the path under the hostname."
+  type = list(object({
+    destination_host       = string
+    destination_path       = string
+    redirect_response_code = string
+    source_paths           = list(string)
+  }))
+}
+
+variable "ssl_policy" {
+  default     = null
+  description = "The SSL policy to use for the redirects."
+  type        = string
+}
